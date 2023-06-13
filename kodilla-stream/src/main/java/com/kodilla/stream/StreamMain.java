f77755d5b98ff6ca7f9c78f8afe79de03ba5d3d8
Kamil Radliński <radlin@MacBook-Air-Kamil.local>
2023-06-13 10:49:31 +0200

    7.2. Czym jest Stream?

--- kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
+++ kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
@@ -1,18 +1,20 @@
 package com.kodilla.stream;
 
 import com.kodilla.stream.beautifier.PoemBeautifier;
+import com.kodilla.stream.iterate.NumbersGenerator;
 import com.kodilla.stream.lambda.ExpressionExecutor;
 import com.kodilla.stream.reference.FunctionalCalculator;
 
 public class StreamMain {
 
     public static void main(String[] args) {
-        PoemBeautifier poemBeautifier = new PoemBeautifier();
-        poemBeautifier.beautify("KAMIL",String::toLowerCase);
-        poemBeautifier.beautify("Marta",String::toUpperCase);
-        poemBeautifier.beautify("Kasia",text -> text + "aaaaa");
-        poemBeautifier.beautify("Barbara",text -> text + "ABC");
-        poemBeautifier.beautify("Basia",text ->"ABC" + text);
-
+//        PoemBeautifier poemBeautifier = new PoemBeautifier();
+//        poemBeautifier.beautify("KAMIL",String::toLowerCase);
+//        poemBeautifier.beautify("Marta",String::toUpperCase);
+//        poemBeautifier.beautify("Kasia",text -> text + "aaaaa");
+//        poemBeautifier.beautify("Barbara",text -> text + "ABC");
+//        poemBeautifier.beautify("Basia",text ->"ABC" + text);
+        System.out.println("Using Stream to generate even numbers from 1 to 20");
+        NumbersGenerator.generateEven(20);
     }
 }
