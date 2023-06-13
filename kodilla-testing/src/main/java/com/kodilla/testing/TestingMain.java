27fb179082a7cb337bb7277ab55a2ae09264d7d1
Kamil Radliński <radlin@MacBook-Air-Kamil.local>
2023-06-13 09:44:30 +0200

    7.1. Wstęp do programowania funkcyjnego, czyli wyrażenia lambda

--- kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
@@ -1,7 +1,6 @@
 package com.kodilla.testing;
 
 import com.kodilla.testing.calculator.Calculator;
-import com.kodilla.testing.collection.OddNumbersExterminator;
 import com.kodilla.testing.user.SimpleUser;
 
 import java.util.ArrayList;
